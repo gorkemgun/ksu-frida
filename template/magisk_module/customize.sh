3f44d9bbfb85e1100d1d7c8688fca57152a55c7b
Gorkem Gun <gorkem.gun@ieee.metu.edu.tr>
2026-04-16 18:42:00 +0300

    remove Riru support and rewrite README
    - Remove Riru flavor, build config, native code and scripts
    - Keep only Zygisk for KernelSU/Magisk
    - Rewrite README with updated usage guide

--- template/magisk_module/customize.sh
+++ template/magisk_module/customize.sh
@@ -1,15 +1,8 @@
 SKIPUNZIP=1
 
-FLAVOR=@FLAVOR@
 MODULE_ID=@MODULE_ID@
 
 TMP_MODULE_DIR=/data/local/tmp/libsec
-
-if [ "$FLAVOR" != "zygisk" ] && [ "$FLAVOR" != "riru" ]; then
-  abort "! Unknown ZygiskFrida flavor: $FLAVOR"
-else
-  ui_print "- ZygiskFrida flavor: $FLAVOR"
-fi
 
 if [ "$ARCH" != "arm" ] && [ "$ARCH" != "arm64" ] && [ "$ARCH" != "x86" ] && [ "$ARCH" != "x64" ]; then
   abort "! Unsupported platform: $ARCH"
@@ -35,15 +28,6 @@ mkdir -p "$MODPATH/webroot"
 extract "$ZIPFILE" 'webroot/index.html' "$MODPATH/webroot" true
 extract "$ZIPFILE" 'webroot/main.js' "$MODPATH/webroot" true
 
-if [ "$FLAVOR" = "riru" ]; then
-  ui_print "- Extracting riru.sh"
-  extract "$ZIPFILE" 'riru.sh' "$TMPDIR"
-  . $TMPDIR/riru.sh
-
-  check_riru_version
-  enforce_install_from_magisk_app
-fi
-
 LIB32_NAME="armeabi-v7a.so"
 LIB64_NAME="arm64-v8a.so"
 LIB32_DEST="$MODPATH/zygisk"
@@ -64,8 +48,6 @@ fi
 
 ui_print "- Using busybox: $BUSYBOX_BIN"
 
-[ "$FLAVOR" = "riru" ] && LIB32_DEST="$MODPATH/riru/lib"
-[ "$FLAVOR" = "riru" ] && LIB64_DEST="$MODPATH/riru/lib64"
 [ "$ARCH" = "x86" ] || [ "$ARCH" = "x64" ] && LIB32_NAME="x86.so"
 [ "$ARCH" = "x86" ] || [ "$ARCH" = "x64" ] && LIB64_NAME="x86_64.so"
 
@@ -74,12 +56,10 @@ mkdir -p "$LIB64_DEST"
 
 ui_print "- Extracting 32-bit libraries"
 extract "$ZIPFILE" "lib/$LIB32_NAME" "$LIB32_DEST" true
-[ "$FLAVOR" = "riru" ] && mv "$LIB32_DEST/$LIB32_NAME" "${LIB32_DEST}/lib${MODULE_ID}.so"
 
 if [ "$IS64BIT" = true ]; then
   ui_print "- Extracting 64-bit libraries"
   extract "$ZIPFILE" "lib/$LIB64_NAME" "$LIB64_DEST" true
-  [ "$FLAVOR" = "riru" ] && mv "$LIB64_DEST/$LIB64_NAME" "${LIB64_DEST}/lib${MODULE_ID}.so"
 fi
 
 ui_print "- Extracting bundled frida gadget"
@@ -105,8 +85,3 @@ echo '{"interaction":{"type":"listen","address":"0.0.0.0","port":27042}}' > "$TM
 
 set_perm_recursive "$TMP_MODULE_DIR" 0 0 0755 0644
 set_perm_recursive "$MODPATH" 0 0 0755 0644
-
-
-
-
-
